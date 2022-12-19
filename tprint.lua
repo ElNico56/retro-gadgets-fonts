fe759a7fea0d880b2ad600085ca9ff7646b04b2b
Finlay Wright <70151481+SuperIronMan987@users.noreply.github.com>
2022-12-19 00:37:56 +0000

    added optional scaling for text

--- tprint.lua
+++ tprint.lua
@@ -1,4 +1,5 @@
-local function Tprint (chip:VideoChip, pos:vec2, font:SpriteSheet, txt:string, width, height:number)
+local function Tprint (chip:VideoChip, pos:vec2, font:SpriteSheet, txt:string, width, height:number, scale:number)
+    scale = scale or 1
     local line:number = 0
     local cur:number = 0
     for i=1,#txt do
@@ -12,8 +13,9 @@ local function Tprint (chip:VideoChip, pos:vec2, font:SpriteSheet, txt:string, w
         elseif type(width) == "table" then
             w = width[charID] or width._
         else error("Width must be a number or table") end
-        chip:DrawSprite(pos + vec2(cur,0) + vec2(0,height*line), font, x, y, color.white, color.clear)
-        cur += w
+        local basepos = pos + vec2(cur,0) + vec2(0,height*line)
+        chip:RasterSprite(basepos, basepos + (vec2(w,0)*vec2(scale,1)), basepos + (vec2(w,height)*vec2(scale,scale)), basepos + (vec2(0,height)*vec2(1,scale)), font, x, y, color.white, color.clear)
+        cur += w*scale
     end
 end
 return Tprint

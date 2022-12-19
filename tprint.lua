a2ce6db01c9bd25ea596f9ebd672036b30a15083
ElNico56 <meeqnico@gmail.com>
2022-12-19 04:29:31 -0300

    updated tprint with an optional scale argument

--- tprint.lua
+++ tprint.lua
@@ -1,19 +1,21 @@
-local function Tprint (chip:VideoChip, pos:vec2, font:SpriteSheet, txt:string, width, height:number)
-    local line:number = 0
-    local cur:number = 0
-    for i=1,#txt do
-        if txt:sub(i,i) == "\n" then line += 1 cur = -1 end
-        local charID:number = txt:sub(i,i):byte()
-        local x:number = charID%32
-        local y:number = math.floor(charID/32)
-        local w:number
-        if type(width) == "number" then
-            w = width
-        elseif type(width) == "table" then
-            w = width[charID] or width._
-        else error("Width must be a number or table") end
-        chip:DrawSprite(pos + vec2(cur,0) + vec2(0,height*line), font, x, y, color.white, color.clear)
-        cur += w
-    end
+local function Tprint (chip:VideoChip, pos:vec2, font:SpriteSheet, txt:string, width, height:number, scale:number)
+	scale = scale or 1
+	local line:number = 0
+	local cur:number = 0
+	for i=1,#txt do
+		local charID:number = txt:sub(i,i):byte()
+		local x:number = charID%32
+		local y:number = math.floor(charID/32)
+		local w:number
+		if type(width) == "number" then
+			w = width
+		elseif type(width) == "table" then
+			w = width[charID] or width._
+		else error("Width must be a number or table") end
+		local basepos = pos + vec2(cur,0) + vec2(0,height*scale*line)
+		chip:RasterSprite(basepos, basepos+vec2(8*scale,0), basepos+vec2(8*scale,8*scale), basepos+vec2(0,8*scale), font, x, y, color.white, color.clear)
+		cur += w*scale
+		if txt:sub(i,i) == "\n" then line += 1 cur = -1 end
+	end
 end
 return Tprint
